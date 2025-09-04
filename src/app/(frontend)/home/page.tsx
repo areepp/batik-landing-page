67cd62ab495943ef17f76fdc3a7b907b0bc8a4e9
mferdinandr <mferdinr@gmail.com>
2025-09-04 16:05:52 +0700

    feat: add hero carousel component

---
+++ src/app/(frontend)/home/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HeroCarousel } from './hero'
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen">
+      <main>
+        <HeroCarousel />
+      </main>
+    </div>
+  )
+}
+
+export default HomePage
