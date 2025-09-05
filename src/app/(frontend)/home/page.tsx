2c69ac4d42533199738ab0c9f77fd94f604780fa
mferdinandr <mferdinr@gmail.com>
2025-09-05 09:41:36 +0700

    feat: featured product at home

--- src/app/(frontend)/home/page.tsx
+++ src/app/(frontend)/home/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { HeroCarousel } from './hero'
+import ProductCarousel from './featuredProduct'
 
 const HomePage = () => {
   return (
     <div className="min-h-screen">
       <main>
         <HeroCarousel />
+        <ProductCarousel />
       </main>
     </div>
   )
